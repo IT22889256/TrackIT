e77ac84ef46eacde4938cb889c91f36671b4ebce
nadundilshan0001 <nadundilshanuni01@gmail.com>
2025-03-27 11:45:36 +0530

    commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // Optionally import the services that you want to use
 // import {...} from 'firebase/auth';
 // import {...} from 'firebase/database';
@@ -10,20 +10,20 @@ import { getFirestore } from 'firebase/firestore';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCeLlWvGNwH4pWEJE8cPojqF25oxWt3zS0",
-    authDomain: "trackit-3e5e3.firebaseapp.com",
-    databaseURL: "https://trackit-3e5e3-default-rtdb.firebaseio.com",
-    projectId: "trackit-3e5e3",
-    storageBucket: "trackit-3e5e3.firebasestorage.app",
-    messagingSenderId: "562674376018",
-    appId: "1:562674376018:web:7718363ecf713a14e76e8b",
-    measurementId: "G-0EF0HG3V61"
+  apiKey: "AIzaSyCeLlWvGNwH4pWEJE8cPojqF25oxWt3zS0",
+  authDomain: "trackit-3e5e3.firebaseapp.com",
+  databaseURL: "https://trackit-3e5e3-default-rtdb.firebaseio.com",
+  projectId: "trackit-3e5e3",
+  storageBucket: "trackit-3e5e3.firebasestorage.app",
+  messagingSenderId: "562674376018",
+  appId: "1:562674376018:web:7718363ecf713a14e76e8b",
+  measurementId: "G-0EF0HG3V61",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth ,db};
+export { auth, db };
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
