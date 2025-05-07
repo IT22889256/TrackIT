892941ec23ab452152d416d722e839f4f09f6dff
IT22889256 <127502592+IT22889256@users.noreply.github.com>
2025-05-07 18:10:03 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,24 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';S
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCeLlWvGNwH4pWEJE8cPojqF25oxWt3zS0",
-  authDomain: "trackit-3e5e3.firebaseapp.com",
-  databaseURL: "https://trackit-3e5e3-default-rtdb.firebaseio.comS",
-  projectId: "trackit-3e5e3",
-  storageBucket: "trackit-3e5e3.firebasestorage.app",
-  messagingSenderId: "562674376018",
-  appId: "1:562674376018:web:7718363ecf713a14e76e8b",
-  measurementId: "G-0EF0HG3V61",
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
